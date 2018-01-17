a3cb9cc6ca62682fcbeaf72c046d14f75e76b9d6
Patryk Konior <patryk@arteia.co>
2018-01-17 19:21:25 +0100

    fix try 4

--- src/components/services/MenuItemsService.js
+++ src/components/services/MenuItemsService.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 class MenuItemsService {
     constructor(props) {
         if (process.env.REACT_APP_NODE_ENV === 'production') {
-            this.ORDERS_API_URL = 'https://iosr2017orders-production.herokuapp.com/v1/menu/items';
+            this.ORDERS_API_URL = 'https://iosr2017menu-production.herokuapp.com/v1/menu/items';
         } else if (process.env.REACT_APP_NODE_ENV === 'staging') {
-            this.ORDERS_API_URL = 'https://iosr2017orders-staging.herokuapp.com/v1/menu/items';
+            this.ORDERS_API_URL = 'https://iosr2017menu-staging.herokuapp.com/v1/menu/items';
         } else {
             this.ORDERS_API_URL = 'http://localhost:3002/v1/menu/items';
         }
