2c14701c6f5e663b25e83c36ea504d3a2b44b51a
Patryk Konior <patryk@arteia.co>
2018-01-17 18:06:32 +0100

    api urls based on env

--- src/components/services/MenuItemsService.js
+++ src/components/services/MenuItemsService.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
+import _ from 'lodash';
 
 class MenuItemsService {
     constructor(props) {
-        this.ORDERS_API_URL = 'http://localhost:3002/v1/menu/items';
+        const NODE_ENV = _.get(process, 'env', 'local');
+        if (NODE_ENV === 'production') {
+            this.ORDERS_API_URL = 'http://iosr2017orders-production/v1/menu/items';
+        } else if (NODE_ENV === 'staging') {
+            this.ORDERS_API_URL = 'http://iosr2017orders-staging/v1/menu/items';
+        } else {
+            this.ORDERS_API_URL = 'http://localhost:3002/v1/menu/items';
+        }
     }
     getById(id) {
         return axios.get(`${this.ORDERS_API_URL}/${id}`)
