ff28ccebafae629a32442d900b919e569d53a4d0
Patryk Konior <patryk@arteia.co>
2018-01-17 19:14:12 +0100

    fix try 3

--- src/components/services/MenuItemsService.js
+++ src/components/services/MenuItemsService.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-import _ from 'lodash';
 
 class MenuItemsService {
     constructor(props) {
-        const REACT_APP_NODE_ENV = _.get(process, 'env', 'local');
-        if (REACT_APP_NODE_ENV === 'production') {
+        if (process.env.REACT_APP_NODE_ENV === 'production') {
             this.ORDERS_API_URL = 'https://iosr2017orders-production.herokuapp.com/v1/menu/items';
-        } else if (REACT_APP_NODE_ENV === 'staging') {
+        } else if (process.env.REACT_APP_NODE_ENV === 'staging') {
             this.ORDERS_API_URL = 'https://iosr2017orders-staging.herokuapp.com/v1/menu/items';
         } else {
             this.ORDERS_API_URL = 'http://localhost:3002/v1/menu/items';
