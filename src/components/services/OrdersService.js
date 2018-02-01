5adc3a59b131f4119614811d48a959c1e8de555c
Patryk Konior <patryk@arteia.co>
2018-02-01 14:06:51 +0100

    fixed orders url

--- src/components/services/OrdersService.js
+++ src/components/services/OrdersService.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 class OrdersService {
     constructor(props) {
         if (process.env.REACT_APP_NODE_ENV === 'production') {
-            this.ORDERS_API_URL = 'https://iosr2017orders.herokuapp.com';
+            this.ORDERS_API_URL = 'https://iosr2017orders-production.herokuapp.com';
         } else if (process.env.REACT_APP_NODE_ENV === 'staging') {
-            this.ORDERS_API_URL = 'https://iosr2017orders.herokuapp.com';
+            this.ORDERS_API_URL = 'https://iosr2017orders-staging.herokuapp.com';
         } else {
             this.ORDERS_API_URL = 'http://localhost:3003';
         }
